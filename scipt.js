1dcd130e42c544762c3f980deaaabdec0caee8e6
dadinelli <dadi.martinelli@gmail.com>
2025-10-28 10:00:22 +0100

    fixed width slide

--- scipt.js
+++ scipt.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buttons.forEach(button => {
     button.addEventListener("click", () => {
-      const offset = button.dataset.carouselButton === "next" ? 3 : -3;
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
       const slides = [...carousel.querySelectorAll(".slide")];
       const activeSlide = carousel.querySelector("[data-active]");
       let newIndex = slides.indexOf(activeSlide) + offset;
